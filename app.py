70b2a5a7a5af8b40cf6afa7fd21647e63ef6d6d0
kedar <kedarnathjani421@gmail.com>
2025-08-25 13:52:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,7 @@ def predict():
     return render_template('index.html', prediction_text=prediction_text)
 
 if __name__ == '__main__':
+    import os
+    part=int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port,debug=False)
     app.run(debug=True)
